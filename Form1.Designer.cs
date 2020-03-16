9ee055b75372358b70bb4c8725ac0b3681351ba6
Vitaly Chupaev <vitaly.chupaev@arcadia.spb.ru>
2020-03-16 13:37:22 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace DbChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.connStrComboBox = new System.Windows.Forms.ComboBox();
+            this.queryTextbox = new System.Windows.Forms.TextBox();
+            this.resultsTextbox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.connStrTextBox = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.saveButton = new System.Windows.Forms.Button();
+            this.wipeHistoryButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connStrComboBox
+            // 
+            this.connStrComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.connStrComboBox.FormattingEnabled = true;
+            this.connStrComboBox.Location = new System.Drawing.Point(12, 12);
+            this.connStrComboBox.Name = "connStrComboBox";
+            this.connStrComboBox.Size = new System.Drawing.Size(791, 21);
+            this.connStrComboBox.TabIndex = 0;
+            this.connStrComboBox.SelectedIndexChanged += new System.EventHandler(this.connStrComboBox_SelectedIndexChanged);
+            // 
+            // queryTextbox
+            // 
+            this.queryTextbox.AcceptsReturn = true;
+            this.queryTextbox.AcceptsTab = true;
+            this.queryTextbox.AllowDrop = true;
+            this.queryTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.queryTextbox.Location = new System.Drawing.Point(0, 0);
+            this.queryTextbox.MaxLength = 1048576;
+            this.queryTextbox.Multiline = true;
+            this.queryTextbox.Name = "queryTextbox";
+            this.queryTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.queryTextbox.Size = new System.Drawing.Size(872, 191);
+            this.queryTextbox.TabIndex = 2;
+            this.queryTextbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.queryTextbox_KeyUp);
+            // 
+            // resultsTextbox
+            // 
+            this.resultsTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsTextbox.Location = new System.Drawing.Point(0, 3);
+            this.resultsTextbox.Multiline = true;
+            this.resultsTextbox.Name = "resultsTextbox";
+            this.resultsTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTextbox.Size = new System.Drawing.Size(872, 300);
+            this.resultsTextbox.TabIndex = 3;
+            // 
+            // runButton
+            // 
+            this.runButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.runButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.runButton.Location = new System.Drawing.Point(809, 10);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Run";
+            this.toolTip1.SetToolTip(this.runButton, "Run the query");
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // connStrTextBox
+            // 
+            this.connStrTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.connStrTextBox.Location = new System.Drawing.Point(12, 39);
+            this.connStrTextBox.Multiline = true;
+            this.connStrTextBox.Name = "connStrTextBox";
+            this.connStrTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.connStrTextBox.Size = new System.Drawing.Size(791, 77);
+            this.connStrTextBox.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 141);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.queryTextbox);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.resultsTextbox);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(872, 497);
+            this.splitContainer1.SplitterDistance = 190;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(788, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Save to history";
+            this.toolTip1.SetToolTip(this.checkBox1, "Navigate using Alt + Arrow (Left and Right)");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.saveButton.Location = new System.Drawing.Point(809, 37);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.toolTip1.SetToolTip(this.saveButton, "Add or update connection string");
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // wipeHistoryButton
+            // 
+            this.wipeHistoryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.wipeHistoryButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.wipeHistoryButton.Location = new System.Drawing.Point(809, 93);
+            this.wipeHistoryButton.Name = "wipeHistoryButton";
+            this.wipeHistoryButton.Size = new System.Drawing.Size(75, 23);
+            this.wipeHistoryButton.TabIndex = 9;
+            this.wipeHistoryButton.Text = "Wipe history";
+            this.toolTip1.SetToolTip(this.wipeHistoryButton, "Delete history from file");
+            this.wipeHistoryButton.UseVisualStyleBackColor = true;
+            this.wipeHistoryButton.Click += new System.EventHandler(this.wipeHistoryButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.deleteButton.Location = new System.Drawing.Point(809, 64);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 10;
+            this.deleteButton.Text = "Delete";
+            this.toolTip1.SetToolTip(this.deleteButton, "Delete connection string except those from machine.config");
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 122);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(770, 13);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.runButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 650);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.wipeHistoryButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.connStrTextBox);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.connStrComboBox);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DbChecker";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox connStrComboBox;
+        private System.Windows.Forms.TextBox queryTextbox;
+        private System.Windows.Forms.TextBox resultsTextbox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox connStrTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button wipeHistoryButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace DbChecker
+{
+    public partial class Form1 : Form
+    {
+        private const string delimiter = ">>>>>>>";
+        private int historyPosition = 0;
+        List<string> historyList = new List<string>();
+        private CancellationTokenSource cts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddUsers();
+            LoadHistory();
+        }
+
+        private void LoadHistory()
+        {
+            if (!File.Exists("queries.txt"))
+            {
+                File.CreateText("queries.txt").Close();
+            }
+
+            var sb = new StringBuilder();
+            foreach (var line in File.ReadLines("queries.txt"))
+            {
+                if (line.Trim() != delimiter)
+                {
+                    sb.AppendLine(line);
+                }
+                else
+                {
+                    var query = sb.ToString().Trim();
+                    if (query.Length > 0)
+                    {
+                        historyList.Add(sb.ToString());
+                    }
+
+                    sb.Clear();
+                }
+            }
+
+            if (historyList.Any())
+                queryTextbox.Text = historyList[0];
+        }
+
+        private void SaveHistory()
+        {
+            if (!checkBox1.Checked)
+                return;
+
+            if (!historyList.Any(i => i.Trim().ToLower().Equals(queryTextbox.Text.Trim().ToLower())))
+            {
+                File.AppendAllText(
+                    "queries.txt",
+                    $"{queryTextbox.Text}{Environment.NewLine}{delimiter}{Environment.NewLine}");
+
+                historyList.Add(queryTextbox.Text);
+            }
+        }
+
+        private void AddUsers()
+        {
+            connStrComboBox.Items.Clear();
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            connStrComboBox.Items.AddRange(ConfigurationManager.ConnectionStrings.Cast<ConnectionStringSettings>().ToArray());
+            connStrComboBox.DisplayMember = "Name";
+            connStrComboBox.Focus();
+
+            connStrComboBox.SelectedIndex = Convert.ToInt32(ConfigurationManager.AppSettings.Get("lastConnStrPosition"));
+        }
+
+        #region Event handlers
+
+        private async void runButton_Click(object sender, EventArgs e)
+        {
+            if (runButton.Tag == "cancel")
+            {
+                cts?.Cancel();
+                return;
+            }
+
+            SaveHistory();
+
+            cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var progressHandler = new Progress<string>(value =>
+            {
+                resultsTextbox.AppendText(value.ToString());
+            });
+
+            var progress = progressHandler as IProgress<string>;
+            var sb = new StringBuilder();
+
+            try
+            {
+                runButton.Text = "Cancel";
+                runButton.Tag = "cancel";
+                progressBar1.Visible = true;
+
+                await Task.Run(() =>
+                {
+                    var connStr = connStrTextBox.Text ??
+                                  ((ConnectionStringSettings)connStrComboBox.SelectedItem).ConnectionString;
+                    using (var connection = new SqlConnection(connStr))
+                    {
+                        var cmd = new SqlCommand(queryTextbox.Text, connection);
+                        cmd.CommandTimeout = 3600;
+
+                        connection.Open();
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            var columnNames = Enumerable.Range(0, reader.FieldCount)
+                                .Select(reader.GetName)
+                                .ToList();
+
+                            sb.Append(string.Join(",", columnNames));
+                            sb.AppendLine();
+
+                            while (reader.Read())
+                            {
+                                token.ThrowIfCancellationRequested();
+
+                                for (var i = 0; i < reader.FieldCount; i++)
+                                {
+                                    var value = reader[i].ToString();
+                                    if (value.Contains(","))
+                                        value = "\"" + value + "\"";
+
+                                    sb.Append(value.Replace(Environment.NewLine, " ") + ",");
+                                }
+
+                                sb.Length--;
+                                sb.AppendLine();
+                            }
+                        }
+                    }
+                });
+                resultsTextbox.AppendText(sb.ToString());
+                resultsTextbox.AppendText(Environment.NewLine);
+            }
+            catch (OperationCanceledException)
+            {
+                resultsTextbox.AppendText(sb.ToString());
+                resultsTextbox.AppendText(Environment.NewLine);
+                resultsTextbox.AppendText("\r\nCancelled.\r\n");
+            }
+            catch (Exception ex)
+            {
+                resultsTextbox.AppendText(ex.Message);
+                resultsTextbox.AppendText(Environment.NewLine);
+            }
+
+            runButton.Text = "Run";
+            runButton.Tag = string.Empty;
+            progressBar1.Visible = false;
+        }
+
+        private void connStrComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            connStrTextBox.Text = ((ConnectionStringSettings)connStrComboBox.SelectedItem).ConnectionString;
+
+            var connStrIndex = connStrComboBox.SelectedIndex;
+            var doc = XDocument.Load(ConfigurationManager.AppSettings.Get("config"));
+            var lastConnStrPosition = doc.XPathSelectElement("//appSettings/add[@key='lastConnStrPosition']");
+            lastConnStrPosition?.SetAttributeValue("value", connStrIndex);
+            doc.Save(ConfigurationManager.AppSettings.Get("config"));
+        }
+
+        private void queryTextbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!historyList.Any()) return;
+
+            if (e.Modifiers == Keys.Alt && e.KeyCode == Keys.Right)
+            {
+                if (historyPosition == historyList.Count - 1)
+                {
+                    historyPosition = 0;
+                }
+                else
+                {
+                    historyPosition++;
+                }
+
+                queryTextbox.Text = historyList[historyPosition];
+            }
+
+            if (e.Modifiers == Keys.Alt && e.KeyCode == Keys.Left)
+            {
+                if (historyPosition == 0)
+                {
+                    historyPosition = historyList.Count - 1;
+                }
+                else
+                {
+                    historyPosition--;
+                }
+
+                queryTextbox.Text = historyList[historyPosition];
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var connStrName = (connStrComboBox.SelectedItem as ConnectionStringSettings)?.Name ?? connStrComboBox.Text;
+            if (connStrName != null)
+            {
+                var doc = XDocument.Load(ConfigurationManager.AppSettings.Get("config"));
+                var connStrs = doc.XPathSelectElement("//connectionStrings");
+                var connStr = doc.XPathSelectElement($"//connectionStrings/add[@name='{connStrName}']");
+                if (connStr == null)
+                {
+                    connStr = new XElement("add",
+                        new XAttribute("name", connStrName),
+                        new XAttribute("connectionString", connStrTextBox.Text));
+                    connStrs.Add(connStr);
+                }
+                else
+                {
+                    connStr.SetAttributeValue("connectionString", connStrTextBox.Text);
+                }
+
+                doc.Save(ConfigurationManager.AppSettings.Get("config"));
+                AddUsers();
+                connStrComboBox.SelectedIndex = connStrComboBox.FindStringExact(connStrName);
+            }
+        }
+
+        private void wipeHistoryButton_Click(object sender, EventArgs e)
+        {
+            File.Delete("queries.txt");
+            File.Create("queries.txt").Close();
+            historyList.Clear();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            var connStrName = (connStrComboBox.SelectedItem as ConnectionStringSettings)?.Name ?? connStrComboBox.Text;
+            if (connStrName != null)
+            {
+                var doc = XDocument.Load(ConfigurationManager.AppSettings.Get("config"));
+                var connStr = doc.XPathSelectElement($"//connectionStrings/add[@name='{connStrName}']");
+                connStr?.Remove();
+
+                doc.Save(ConfigurationManager.AppSettings.Get("config"));
+                AddUsers();
+            }
+        }
+
+        #endregion
+    }
+}
